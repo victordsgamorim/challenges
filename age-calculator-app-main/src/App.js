ccea17d5c6bb7512b95e38105d3633f41ffb88c2
Victor Amorim <victordsgamorim@gmail.com>
2024-03-14 17:04:57 +0000

    age calculator app challenge

---
+++ age-calculator-app-main/src/App.js
@@ -0,0 +1,11 @@
+import TextField from "./components/TextField";
+
+function App() {
+  return (
+    <div className="App">
+      <TextField />
+    </div>
+  );
+}
+
+export default App;
---
+++ age-calculator-app-main/src/components/TextField/index.js
@@ -0,0 +1,12 @@
+import './TextField.css';
+
+const TextField = () => {
+    return (
+        <div>
+            <label for='name'>Name</label>
+            <input id='name' />
+        </div>
+    );
+}
+
+export default TextField;
